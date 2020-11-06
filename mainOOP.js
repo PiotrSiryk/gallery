a611fcaf0d87a0f3306b1f4f2a5d7977f3ed2942
PiotrSiryk <Piotr.Siryk@gmail.com>
2020-11-06 23:00:59 +0100

    final?

--- mainOOP.js
+++ mainOOP.js
@@ -1,20 +1,22 @@
+const displayWindow1 = document.querySelector('.blackout1');
+const displayWindow2 = document.querySelector('.blackout2');
+
 class Gallery {
-    constructor(type) {
-
-        this.main = document.querySelector('.main');
-        this.blackout = document.querySelector('.blackout');
-        this.blackoutX = document.querySelector('.blackout > .x-div');
+    constructor(blackout, type, elements) {
+        this.blackout = blackout;
+        this.blackoutX = blackout.querySelector('.x-div');
+        this.rest = blackout.querySelector('.rest');
+        this.right = blackout.querySelector('.right');
+        this.left = blackout.querySelector('.left');
+        this.img = blackout.querySelector('.imgDiv>img');
+        
+        this.elements = document.querySelector(elements)
         this.images = document.querySelectorAll(`[data-type="${type}"]`);
-        this.rest = document.querySelector('.rest');
-        this.img = document.querySelector('.imgDiv>img');
 
-        this.right = document.querySelector('.right')
-        this.left = document.querySelector('.left')
-
-        this.blackoutX.addEventListener('click', this.off.bind(this))
+        this.blackoutX.addEventListener('click', this.off.bind(this));
         this.images.forEach(element => {element.addEventListener('click', this.display.bind(this))});
-        this.right.addEventListener('click', this.toTheRight.bind(this))
-        this.left.addEventListener('click', this.toTheLeft.bind(this))
+        this.right.addEventListener('click', this.toTheRight.bind(this));
+        this.left.addEventListener('click', this.toTheLeft.bind(this));
     }
     off() {
         this.blackout.style.display ="none";
@@ -24,28 +26,29 @@ class Gallery {
         this.img.src = event.target.src;
         this.rest.innerHTML = "";
         this.images.forEach(element => {this.rest.insertAdjacentHTML('beforeend', `<img src="${element.src}" data-type="${element.dataset.type}" data-id="${element.dataset.id}" alt="">`)})
-        
+
         this.img.dataset.id = event.target.dataset.id;
 
         /* bottom bar */
-        this.mini = document.querySelectorAll('.rest>*');
-        this.mini.forEach(element => {element.addEventListener('click', this.change.bind(this))});
+        this.mini = this.blackout.querySelectorAll('.rest>*');
+        this.mini.forEach(element => {element.addEventListener('click', this.swap.bind(this))});
     }
-    change(event) {
+    swap(event) {
         this.img.src = event.target.src;
         this.img.dataset.id = event.target.dataset.id;
     }
     toTheRight() {
-        this.img.dataset.id == this.mini.length ? this.img.dataset.id = 0 : null;
-        this.img.src = this.blackout.querySelector(`[data-id="${(Number(this.img.dataset.id)+1).toString()}"]`).src;
+        this.img.dataset.id >= this.mini.length ? this.img.dataset.id = 0 : null;
+        this.img.src = this.elements.querySelector(`[data-id="${(Number(this.img.dataset.id)+1).toString()}"]`).src;
         this.img.dataset.id = (Number(this.img.dataset.id)+1).toString();
     }
     toTheLeft() {
-        this.img.dataset.id == 1 ? this.img.dataset.id = this.mini.length+1 : null;
-        this.img.src = this.blackout.querySelector(`[data-id="${(Number(this.img.dataset.id)-1).toString()}"]`).src;
+        this.img.dataset.id <= 1 ? this.img.dataset.id = this.mini.length+1 : null;
+        this.img.src = this.elements.querySelector(`[data-id="${(Number(this.img.dataset.id)-1).toString()}"]`).src;
         this.img.dataset.id = (Number(this.img.dataset.id)-1).toString();
     }
 }
 
-const galMeme = new Gallery('meme');
-const galCity = new Gallery('city');
+const galCity = new Gallery(displayWindow2, 'city', '.city');
+const galMeme = new Gallery(displayWindow1, 'meme', '.memes');
+console.log(galCity);
